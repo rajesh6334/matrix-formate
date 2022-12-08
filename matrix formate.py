a6dfa6865c1169335aa0ed1c717065cd3031eb38
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-12-08 20:12:46 +0530

    Add files via upload

---
+++ matrix formate.py
@@ -0,0 +1,9 @@
+#mtrix formate
+n=int(input('enter no of rows'))
+m=int(input('enter no coloumns'))
+k=1
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        print(k,end=' ')
+        k=k+1
+    print()
